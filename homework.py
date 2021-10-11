bcf0214b61e122f46f32c162d58d4fe85a7e6688
Marina <margoloko@mail.ru>
2021-10-11 11:42:40 +0700

    version 3

--- homework.py
+++ homework.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 class Record:
     """Класс Record используется для создания данных."""
+
     def __init__(self, amount: float, date: Optional[str] = None,
                  comment: str = "Комментарий осутствует") -> None:
         self.amount = amount
@@ -16,36 +17,41 @@ class Record:
 
 class Calculator:
     """Родительский класс Calculator."""
+
     def __init__(self, limit) -> None:
         self.records = []
         self.limit = limit
         self.today = dt.date.today()
         self.week = self.today - dt.timedelta(weeks=1)
-    """Функция для сохранения новых записей в словарь."""
+
     def add_record(self, record: Record) -> None:
+        """Функция для сохранения новых записей в словарь."""
         self.records.append(record)
-    """Функция рассчитывает сколько денег/каллорий потрачено за сегодня."""
+
     def get_today_stats(self) -> float:
-        sum_for_today = 0
-        return (sum(sum_for_today + i.amount for i in self.records
-                if i.date == self.today))
-    """Функция расчитывает сколько денег/каллорий потрачено
-    за последние 7 дней."""
+        """Функция рассчитывает сколько денег/каллорий потрачено за сегодня."""
+        return (sum(spent_today.amount for spent_today in self.records
+                if spent_today.date == self.today))
+
     def get_week_stats(self) -> float:
-        sum_week = 0
-        return (sum(sum_week + i.amount for i in self.records
-                if self.week < i.date <= self.today))
-    """Функция расчитывает остаток денег/каллорий на сегодня."""
+        """Функция расчитывает сколько денег/каллорий потрачено
+        за последние 7 дней."""
+        return (sum(spent_week.amount for spent_week in self.records
+                if self.week < spent_week.date <= self.today))
+
     def balance_today(self) -> float:
+        """Функция расчитывает остаток денег/каллорий на сегодня."""
         return self.limit - self.get_today_stats()
 
 
 class CaloriesCalculator(Calculator):
     """Класс CaloriesCalculator используется для подсчета каллорий."""
+
     def get_calories_remained(self) -> str:
-        if self.limit > self.get_today_stats():
+        remains = self.balance_today()
+        if self.balance_today() > 0:
             return (f'Сегодня можно съесть что-нибудь ещё, но с общей '
-                    f'калорийностью не более {self.balance_today()} кКал')
+                    f'калорийностью не более {remains} кКал')
         return 'Хватит есть!'
 
 
@@ -54,25 +60,23 @@ class CashCalculator(Calculator):
     USD_RATE = 60.00
     EURO_RATE = 70.00
     RUB_RATE = 1.00
-    """В функции расчитывается сколько ещё денег
-    можно потратить сегодня в рублях, долларах или евро."""
+
     def get_today_cash_remained(self, currency: str) -> str:
+        """В функции расчитывается сколько ещё денег
+        можно потратить сегодня в рублях, долларах или евро."""
         currencies = {'rub': (self.RUB_RATE, 'руб'),
                       'eur': (self.EURO_RATE, 'Euro'),
                       'usd': (self.USD_RATE, 'USD')}
-        try:
-            currency in currencies[currency]
-        except KeyError:
-            return (f'В базе отсутствует валюта {currency}. '
-                    'Используйте другую валюту')
+        if currency not in currencies:
+            raise KeyError(f'В базе отсутствует валюта {currency}. '
+                           'Используйте другую валюту')
+        if not self.balance_today():
+            return 'Денег нет, держись'
         cur_rate, cur_name = currencies[currency]
         cur = round(self.balance_today() / cur_rate, 2)
-        if self.limit == self.get_today_stats():
-            return 'Денег нет, держись'
-        elif self.limit > self.get_today_stats():
+        if self.balance_today() > 0:
             return f'На сегодня осталось {cur} {cur_name}'
-        else:
-            return f'Денег нет, держись: твой долг - {abs(cur)} {cur_name}'
+        return f'Денег нет, держись: твой долг - {abs(cur)} {cur_name}'
 
 
 if __name__ == "__main__":
@@ -97,7 +101,7 @@ if __name__ == "__main__":
     cash_calculator.add_record(Record(amount=3000,
                                       comment='бар в Танин др',
                                       date='08.11.2019'))
-# cash_calculator.add_record(Record(amount=945))
-    print(cash_calculator.get_today_cash_remained('eur'))
+    cash_calculator.add_record(Record(amount=555))
+    print(cash_calculator.get_today_cash_remained('rub'))
 # должно напечататься
 # На сегодня осталось 555 руб
