0e49911b786d5999819b3b641567bdacfae2a142
Balakhonova Marina <90852680+margoloko@users.noreply.github.com>
2023-04-22 12:47:08 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -88,20 +88,12 @@ if __name__ == "__main__":
     cc_calc.add_record(Record(amount=860, comment='lunch'))
     print(cc_calc.get_calories_remained())
 
-
-# создадим калькулятор денег с дневным лимитом 1000
     cash_calculator = CashCalculator(1000)
-# дата в параметрах не указана,
-# так что по умолчанию к записи
-# должна автоматически добавиться сегодняшняя дата
     cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# и к этой записи тоже дата должна добавиться автоматически
     cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# а тут пользователь указал дату, сохраняем её
     cash_calculator.add_record(Record(amount=3000,
                                       comment='бар в Танин др',
                                       date='08.11.2019'))
     cash_calculator.add_record(Record(amount=555))
     print(cash_calculator.get_today_cash_remained('rub'))
-# должно напечататься
-# На сегодня осталось 555 руб
+
